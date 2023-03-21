1a5ba147e176eda473e35a29223f93a1dee0657d
pythonmaster9000 <120974929+pythonmaster9000@users.noreply.github.com>
2023-03-21 13:43:35 -0400

    Update msearch.py

--- msearch.py
+++ msearch.py
@@ -11,23 +11,49 @@ class Search:
             search for the best match between title and tags
     """
 
-    def __init__(self, title, tags):
+    def __init__(self, title, tags=None):
+        if tags is None:
+            tags = []
         self.title = title
         self.tags = tags + [i for i in title.split()]
         self.dbdump = mdatabase.DataBase().grab_all_data()
 
-    def by_title(self):
-        return sorted(self.dbdump, key=lambda e: jarowinkler_similarity(e[1], self.title), reverse=True)
+    def by_speech(self, history=3):
+        matches = []
+        for entry in self.dbdump:
+            speech = entry[3].split()
+            inquiry = self.tags
+            result = -1
+            l = 0
+            r = len(inquiry)
+            while r < len(speech):
+                result = max(jarowinkler_similarity(' '.join(inquiry), ' '.join(speech[l:r])), result)
+                l += 1
+                r += 1
+            matches.append([result, entry])
+        if not history:
+            return sorted(matches, key=lambda e: e[0], reverse=True)
+        return sorted(matches, key=lambda e: e[0], reverse=True)[:history]
 
-    def by_tags(self):
-        return sorted(self.dbdump, key=lambda e: jarowinkler_similarity(e[2], ' '.join(self.tags)), reverse=True)
+    def by_title(self, history=3):
+        if not history:
+            return sorted(self.dbdump, key=lambda e: jarowinkler_similarity(e[1], self.title), reverse=True)
+        return sorted(self.dbdump, key=lambda e: jarowinkler_similarity(e[1], self.title), reverse=True)[:history]
 
-    def search(self):
+    def by_tags(self, history=3):
+        if not history:
+            return sorted(self.dbdump, key=lambda e: jarowinkler_similarity(e[2], ' '.join(self.tags)), reverse=True)
+        return sorted(self.dbdump, key=lambda e: jarowinkler_similarity(e[2], ' '.join(self.tags)), reverse=True)[
+               :history]
+
+    def search(self, history=3):
+        if not history:
+            return sorted(self.dbdump, key=lambda e: max(jarowinkler_similarity(e[2], ' '.join(self.tags)),
+                                                         jarowinkler_similarity(e[1], self.title)), reverse=True)
         return sorted(self.dbdump, key=lambda e: max(jarowinkler_similarity(e[2], ' '.join(self.tags)),
-                                                     jarowinkler_similarity(e[1], self.title)), reverse=True)
+                                                     jarowinkler_similarity(e[1], self.title)), reverse=True)[:history]
 
 
 if __name__ == '__main__':
-    print(Search('terten', ['epic lebron james']).by_title(), 'by title res')
-    print(Search('terten', ['epic lebron james']).by_tags(), 'by title res')
-    print(Search('epic lebron james', ['epic lebron james']).search(), 'by title res')
+    # print(Search('mate').search())
+    #print(Search('mate').by_speech())
